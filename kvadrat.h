c427c5b0e7c37f99961ff72bc361ab8c005f8add
Kalyushin <60959640+Kalyushin@users.noreply.github.com>
2020-03-31 12:44:39 +0300

    Create kvadrat.h

---
+++ kvadrat.h
@@ -0,0 +1,12 @@
+#ifndef KVADRAT_H
+#define KVADRAT_H
+
+typedef struct _roots{
+       int count;
+       double x1;
+       double x2;
+}roots;
+
+roots kvadrat (double a, double b, double c);
+
+#endif // KVADRAT_H
