6d6d797f18322d74a1e4636f91a6a8db44cc580f
Kalyushin <60959640+Kalyushin@users.noreply.github.com>
2020-03-31 12:43:34 +0300

    Update myfunc.h

--- app/myfunc.h
+++ app/myfunc.h
@@ -1,16 +1,8 @@
 #ifndef MYFUNC_H
 #define MYFUNC_H
 
-typedef struct _roots{
-       int count;
-       double x1;
-       double x2;
-}roots;
-
 int myfunc(int b);
 
 int fibonachi(int num);
-
-roots kvadrat (double a, double b, double c);
 
 #endif // MYFUNC_H
